3c14c803a78e5eda706e6138493f050180867fd5
Murillo Caetano <murillo.santos@ucsal.edu.br>
2022-03-25 11:11:21 -0300

    first commit

---
+++ teatro-ucsal/src/test/java/br/ucsal/teatroucsal/TeatroUcsalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.teatroucsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatroUcsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
